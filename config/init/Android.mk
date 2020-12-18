Roynas-Android-Playground/device_samsung_universal9611-common_old

config: init: add samsung modifications from /init.rc
Change-Id: I2056c0ce0eefc8de67c31ea5786ea9773c29984e

config/init/Android.mk
@@ -24,6 +24,13 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := init.exynos9611.root.rc
+LOCAL_SRC_FILES := init.exynos9611.root.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.exynos9611.rc
 LOCAL_SRC_FILES := init.exynos9611.rc
 LOCAL_MODULE_CLASS := ETC

platform/init.mk
@@ -1,5 +1,6 @@
 ### INIT
 PRODUCT_PACKAGES += \
+    init.exynos9611.root.rc \
     init.exynos9611.rc \
     init.exynos9611.usb.rc \
     fstab.exynos9611

synt4x93/android_device_samsung_exynos990-common

Change-Id: Ie8ca7fee1c22ec54d0332b24c79879bcbba43f19

config/init/Android.mk
@@ -24,6 +24,13 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := init.exynos990.root.rc
+LOCAL_SRC_FILES := init.exynos990.root.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.exynos990.rc
 LOCAL_SRC_FILES := init.exynos990.rc
 LOCAL_MODULE_CLASS := ETC

platform/init.mk
@@ -1,5 +1,6 @@
 ### INIT
 PRODUCT_PACKAGES += \
+    init.exynos990.root.rc \
     init.exynos990.rc \
     init.exynos990.usb.rc \
     fstab.exynos990
e0e6c3387c4bea8ee642e7eca344c828e88ea363
Tim Zimmermann <tim@linux4.de>
2020-08-22 06:15:26 +0200

    config: init: add samsung modifications from /init.rc
    Change-Id: Ie8ca7fee1c22ec54d0332b24c79879bcbba43f19

--- config/init/Android.mk
+++ config/init/Android.mk
@@ -24,6 +24,13 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := init.exynos990.root.rc
+LOCAL_SRC_FILES := init.exynos990.root.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.exynos990.rc
 LOCAL_SRC_FILES := init.exynos990.rc
 LOCAL_MODULE_CLASS := ETC
--- platform/init.mk
+++ platform/init.mk
@@ -1,5 +1,6 @@
 ### INIT
 PRODUCT_PACKAGES += \
+    init.exynos990.root.rc \
     init.exynos990.rc \
     init.exynos990.usb.rc \
     fstab.exynos990
