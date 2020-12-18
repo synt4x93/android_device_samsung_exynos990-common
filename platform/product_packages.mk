Roynas-Android-Playground/device_samsung_universal9611-common_old

hardware: interfaces: import usb hal from exynos9820-common
Change-Id: Ifae0f7d72460645c57b76d660dbcec3cdbb6ffa7

platform/product_packages.mk
@@ -130,10 +130,14 @@ PRODUCT_PACKAGES += \
     libshim_stagefright_foundation \
     libshim_sensorndkbridge
 
-## SOUNDTRIGGER
+### SOUNDTRIGGER
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.0-service
+
+### USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.1-service.exynos9611
 
 ### USB TRUST HAL
 PRODUCT_PACKAGES += \

synt4x93/android_device_samsung_exynos990-common

platform/product_packages.mk
@@ -142,10 +142,14 @@ PRODUCT_PACKAGES += \
     libshim_stagefright_foundation \
     libshim_sensorndkbridge
 
-## SOUNDTRIGGER
+### SOUNDTRIGGER
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.0-service
+
+### USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.1-service.exynos990
 
 ### USB TRUST HAL
 PRODUCT_PACKAGES += \