418885fc4199afc2ab10ec97f946f6a4e759c6d7
Michael Bestas <mkbestas@lineageos.org>
2021-01-01 04:57:09 +0200

    exynos990-common: Remove android.hardware.graphics.composer@2.{1,2}-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -70,9 +70,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.2-impl \
-    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.composer@2.2-service
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
