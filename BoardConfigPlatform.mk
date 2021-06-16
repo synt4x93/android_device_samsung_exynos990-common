4cf562845ad830dcfbc2206adcf7abb547687986
Tim Zimmermann <tim@linux4.de>
2021-06-16 11:14:55 +0200

    exynos990-common: clean up BoardConfig
    Change-Id: Icf42434df2178761465edbc15a57595e4ceff483

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,166 +13,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+COMMON_PATH := device/samsung/exynos990-common
+
+BUILD_BROKEN_DUP_RULES := true
+
+## Include path
+TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/hardware/include
+
+## Inherit proprietary vendor configuartion
 include vendor/samsung/exynos990-common/BoardConfigVendor.mk
 
-BUILD_TOP := $(shell pwd)
-
-PLATFORM_PATH := device/samsung/exynos990-common
-
-### BOARD
-TARGET_BOARD_PLATFORM := universal990
-TARGET_SLSI_VARIANT := bsp
-TARGET_SOC := exynos990
-TARGET_BOOTLOADER_BOARD_NAME := exynos990
-TARGET_BOARD_PLATFORM_GPU := mali-g77
-
-# build/make/core/Makefile
-TARGET_NO_BOOTLOADER := true
-
-# Enable hardware/samsung
-BOARD_VENDOR := samsung
-
-### PROCESSOR
+## Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 
+## Architecture (Secondary)
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 
-### RELASETOOLS
-TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)/releasetools
-
-### RENDERSCRIPT
-OVERRIDE_RS_DRIVER := libRSDriverArm.so
-
-### HARDWARE INCLUDE
-TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/hardware/include
-
-### KERNEL
-TARGET_KERNEL_SOURCE = kernel/samsung/exynos990/
-TARGET_KERNEL_CLANG_COMPILE := true
-
-BOARD_CUSTOM_BOOTIMG         := true
-BOARD_CUSTOM_BOOTIMG_MK      := $(PLATFORM_PATH)/kernel/mkbootimg.mk
-BOARD_KERNEL_BASE            := 0x10000000
-# See `bbootimg -i boot.img`
-BOARD_KERNEL_PAGESIZE        := 2048
-# Looks like Samsung's sboot doesn't suppored a zipped Kernel
-BOARD_KERNEL_IMAGE_NAME      := Image
-# Build a device tree overlay
-BOARD_KERNEL_SEPARATED_DTBO  := true
-BOARD_DTBO_CFG               := $(PLATFORM_PATH)/kernel/$(TARGET_DEVICE).cfg
-
-# See `bbootimg -i boot.img`
-BOARD_DTB_OFFSET             := 0x00000000
-BOARD_KERNEL_OFFSET          := 0x00008000
-BOARD_KERNEL_RAMDISK_OFFSET  := 0x01000000
-BOARD_KERNEL_TAGS_OFFSET     := 0x00000100
-
-# See `bbootimg -i boot.img`
-BOARD_BOOT_HEADER_VERSION    := 2
-BOARD_BOOT_HEADER_NAME       := SRPSJ28B005KU
-
-BOARD_MKBOOTIMG_ARGS := --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_KERNEL_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --board $(BOARD_BOOT_HEADER_NAME)
-
-### BINDER
-# build/make/core/config.mk
-TARGET_USES_64_BIT_BINDER := true
-
-### VNDK
-BOARD_VNDK_VERSION := current
-
-### SYSTEM
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-# system/core and build/make
-AB_OTA_UPDATER := false
-
-BOARD_ROOT_EXTRA_FOLDERS := \
-    efs \
-    dqmdbg \
-    omr \
-    prism \
-    optics \
-    keydata \
-    keyrefuge
-
-# /metadata
-BOARD_USES_METADATA_PARTITION := true
-
-### SYSTEM_EXT
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_SYSTEM_EXT := system_ext
-
-### VENDOR
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-
-### ODM
-BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_ODM := odm
-
-### PRODUCT
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_PRODUCT := product
-
-### CACHE
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-
-### USERDATA
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-### AUDIO
-USE_XML_AUDIO_POLICY_CONF := 1
-
-### CHARGER
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-### BLUETOOTH
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_PATH)/hardware/bluetooth
-BOARD_CUSTOM_BT_CONFIG := $(PLATFORM_PATH)/hardware/bluetooth/libbt_vndcfg.txt
+## Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/hardware/bluetooth
+BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/hardware/bluetooth/libbt_vndcfg.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-### GRAPHICS
-# hardware/interfaces/configstore/1.1/default/surfaceflinger.mk
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_HAS_HDR_DISPLAY := true
-TARGET_HAS_WIDE_COLOR_DISPLAY := true
+## Boot Image
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/kernel/mkbootimg.mk
+BOARD_DTB_OFFSET := 0x00000000
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_TAGS_OFFSET := 0x00000100
 
-### HIDL
-DEVICE_MANIFEST_FILE += $(PLATFORM_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
+BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
-### KEYMASTER
-TARGET_KEYMASTER_VARIANT := samsung
-
-### SEPOLICY
-include device/lineage/sepolicy/exynos/sepolicy.mk
-
-BOARD_SEPOLICY_TEE_FLAVOR := teegris
-include device/samsung_slsi/sepolicy/sepolicy.mk
-
-BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-
-### PARTITIONS
-BOARD_FLASH_BLOCK_SIZE := 4096
-BOARD_BOOTIMAGE_PARTITION_SIZE := 61865984
-BOARD_CACHEIMAGE_PARTITION_SIZE := 629145600
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 69009408
-BOARD_DTBOIMG_PARTITION_SIZE := 8388608
-
-# Dynamic Partitions
+## Dynamic Partitions
 BOARD_SUPER_PARTITION_SIZE := 9437184000
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9432989696
@@ -192,63 +81,104 @@ BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 1258291200
 BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT := -1
 endif
 
-### PROPERTIES
-TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
-TARGET_PRODUCT_PROP += $(PLATFORM_PATH)/product.prop
-TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
+## DTBO
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_DTBO_CFG := $(COMMON_PATH)/kernel/$(TARGET_DEVICE).cfg
 
-### ANDROID VERIFIED BOOT
-BOARD_AVB_ENABLE := true
-ifeq ($(BOARD_AVB_ENABLE), true)
-ifneq ($(LINEAGE_AVB_KEY_PATH),)
-BOARD_AVB_KEY_PATH := $(LINEAGE_AVB_KEY_PATH)
-else
-BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-endif
-BOARD_AVB_RECOVERY_KEY_PATH := $(BOARD_AVB_KEY_PATH)
-BOARD_AVB_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_RECOVERY_ALGORITHM := $(BOARD_AVB_ALGORITHM)
-BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(BOARD_AVB_ROLLBACK_INDEX)
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --algorithm NONE
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-endif # BOARD_AVB_ENABLE
+## Filesystem
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_ODM := odm
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
-### WIFI
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+## Kernel
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos990
 
-BOARD_WLAN_BCMDHD_SAE := true
+## Keymaster
+TARGET_KEYMASTER_VARIANT := samsung
 
-WIFI_RESET_IFACE_AFTER_MAC_CHANGE := true
-WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
-WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+## Manifest
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
-# hardware/broadcom/wlan/bcmdhd/config/Android.mk
-# external/wpa_supplicant_8/Android.mk
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+## Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 61865984
+BOARD_CACHEIMAGE_PARTITION_SIZE := 629145600
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 69009408
 
-# frameworks/opt/net/wifi/libwifi_hal/Android.mk
-# hardware/samsung/wifiloader/Android.mk
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+BOARD_USES_METADATA_PARTITION := true
 
-### RIL
-# Use stock RIL stack
+BOARD_ROOT_EXTRA_FOLDERS := \
+    efs \
+    dqmdbg \
+    omr \
+    prism \
+    optics \
+    keydata \
+    keyrefuge
+
+## Platform
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := universal990
+TARGET_BOOTLOADER_BOARD_NAME := exynos990
+TARGET_SOC := exynos990
+
+## Properties
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+
+## Recovery
+BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/config/init/fstab.exynos990
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+
+## Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
+
+## RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
-### ALLOW VENDOR FILE OVERRIDE
-BUILD_BROKEN_DUP_RULES := true
-
-### RECOVERY
-BOARD_HAS_DOWNLOAD_MODE := true
-TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
-BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/config/init/fstab.exynos990
-
-### VENDOR SECURITY PATCH DATE
+## Security
 VENDOR_SECURITY_PATCH := 2021-05-01
+
+## SELinux
+BOARD_SEPOLICY_TEE_FLAVOR := teegris
+include device/lineage/sepolicy/exynos/sepolicy.mk
+include device/samsung_slsi/sepolicy/sepolicy.mk
+
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
+## Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+## Wi-Fi
+BOARD_WLAN_BCMDHD_SAE            := true
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+WIFI_RESET_IFACE_AFTER_MAC_CHANGE := true
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
